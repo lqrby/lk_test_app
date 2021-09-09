0181d06decab6bcf6b3eefee952c58061cc708a6
lqrby <1640464937@qq.com>
2021-09-09 19:09:59 +0800

    修改main文件

--- Test_Cases/test_room.py
+++ Test_Cases/test_room.py
@@ -32,81 +32,81 @@ class TestRoomMakeFriends:
                 common_func.save_webImgs("进入推荐聊天室-断言截图")
                 raise a
 
-    @pytest.mark.success
-    @allure.story('进入派对聊天室')
-    @allure.title('进入派对聊天室')
-    def test_party_Room(self, startApp_keepUserData):
-        room_page = RoomPage(startApp_keepUserData)
-        common_func = Cb(startApp_keepUserData)
-        log.info("**************房间模块-派对tap-进退房间****************")
-        room_page.find_room() #点击房间模块
-        room_page.room_tap(RoomPageLocator.party_tap) #点击派对tap
-        room_page.check_MinorSettings() #检测未成年弹框
-        room_page.driver.implicitly_wait(8)
-        liveRoomMum = room_page.open_party_room() #进入派对聊天室
-        with allure.step("进入派对聊天室"):
-            try:
-                assert liveRoomMum == True
-                log.info("进入派对聊天室断言成功")
-            except AssertionError as a:
-                log.error("进入派对聊天室断言失败")
-                common_func.save_webImgs("进入派对聊天室-断言截图")
-                raise a
+    # @pytest.mark.success
+    # @allure.story('进入派对聊天室')
+    # @allure.title('进入派对聊天室')
+    # def test_party_Room(self, startApp_keepUserData):
+    #     room_page = RoomPage(startApp_keepUserData)
+    #     common_func = Cb(startApp_keepUserData)
+    #     log.info("**************房间模块-派对tap-进退房间****************")
+    #     room_page.find_room() #点击房间模块
+    #     room_page.room_tap(RoomPageLocator.party_tap) #点击派对tap
+    #     room_page.check_MinorSettings() #检测未成年弹框
+    #     room_page.driver.implicitly_wait(8)
+    #     liveRoomMum = room_page.open_party_room() #进入派对聊天室
+    #     with allure.step("进入派对聊天室"):
+    #         try:
+    #             assert liveRoomMum == True
+    #             log.info("进入派对聊天室断言成功")
+    #         except AssertionError as a:
+    #             log.error("进入派对聊天室断言失败")
+    #             common_func.save_webImgs("进入派对聊天室-断言截图")
+    #             raise a
 
-    @pytest.mark.success
-    @allure.story('进入开黑聊天室')
-    @allure.title('进入开黑聊天室')
-    def test_openBlack_Room(self, startApp_keepUserData):
-        room_page = RoomPage(startApp_keepUserData)
-        common_func = Cb(startApp_keepUserData)
-        log.info("**************房间模块-开黑tap-进退房间****************")
-        room_page.find_room() #点击房间模块
-        room_page.room_tap(RoomPageLocator.open_black_tap) #点击开黑tap
-        room_page.check_MinorSettings() #检测未成年弹框
-        room_page.driver.implicitly_wait(8)
-        liveRoomMum = room_page.open_black_room() #进入聊天室
-        with allure.step("进入开黑聊天室"):
-            try:
-                assert liveRoomMum == True
-                log.info("进入开黑聊天室断言成功")
-            except AssertionError as a:
-                log.error("进入开黑聊天室断言失败")
-                common_func.save_webImgs("进入开黑聊天室-断言截图")
-                raise a
+    # @pytest.mark.success
+    # @allure.story('进入开黑聊天室')
+    # @allure.title('进入开黑聊天室')
+    # def test_openBlack_Room(self, startApp_keepUserData):
+    #     room_page = RoomPage(startApp_keepUserData)
+    #     common_func = Cb(startApp_keepUserData)
+    #     log.info("**************房间模块-开黑tap-进退房间****************")
+    #     room_page.find_room() #点击房间模块
+    #     room_page.room_tap(RoomPageLocator.open_black_tap) #点击开黑tap
+    #     room_page.check_MinorSettings() #检测未成年弹框
+    #     room_page.driver.implicitly_wait(8)
+    #     liveRoomMum = room_page.open_black_room() #进入聊天室
+    #     with allure.step("进入开黑聊天室"):
+    #         try:
+    #             assert liveRoomMum == True
+    #             log.info("进入开黑聊天室断言成功")
+    #         except AssertionError as a:
+    #             log.error("进入开黑聊天室断言失败")
+    #             common_func.save_webImgs("进入开黑聊天室-断言截图")
+    #             raise a
 
-    @pytest.mark.success
-    @allure.story('创建小窝类型的聊天室')
-    @allure.title('创建一个小窝类型的聊天室')            
-    def test_establish_bathmos_Room(self, startApp_keepUserData):
-        room_page = RoomPage(startApp_keepUserData)
-        common_func = Cb(startApp_keepUserData)
-        log.info("**************创建小窝类型的聊天室****************")
-        liveRoomMum = room_page.found_minNest_room()
-        with allure.step("@创建小窝类型的聊天室@"):
-            try:
-                assert liveRoomMum == True
-                log.info("创建小窝类型的聊天室断言成功")
-            except AssertionError as a:
-                log.error("创建小窝类型的聊天室断言失败")
-                common_func.save_webImgs("创建小窝类型聊天室-断言截图")
-                raise a
+    # @pytest.mark.success
+    # @allure.story('创建小窝类型的聊天室')
+    # @allure.title('创建一个小窝类型的聊天室')            
+    # def test_establish_bathmos_Room(self, startApp_keepUserData):
+    #     room_page = RoomPage(startApp_keepUserData)
+    #     common_func = Cb(startApp_keepUserData)
+    #     log.info("**************创建小窝类型的聊天室****************")
+    #     liveRoomMum = room_page.found_minNest_room()
+    #     with allure.step("@创建小窝类型的聊天室@"):
+    #         try:
+    #             assert liveRoomMum == True
+    #             log.info("创建小窝类型的聊天室断言成功")
+    #         except AssertionError as a:
+    #             log.error("创建小窝类型的聊天室断言失败")
+    #             common_func.save_webImgs("创建小窝类型聊天室-断言截图")
+    #             raise a
 
-    @pytest.mark.success
-    @allure.story('创建萌新接待类型的聊天室')
-    @allure.title('创建一个萌新接待类型的聊天室')            
-    def test_establish_reception_Room(self, startApp_keepUserData):
-        room_page = RoomPage(startApp_keepUserData)
-        common_func = Cb(startApp_keepUserData)
-        log.info("**************创建萌新接待类型的聊天室****************")
-        liveRoomMum = room_page.found_reception_room()
-        with allure.step("@创建萌新接待类型的聊天室@"):
-            try:
-                assert liveRoomMum == True
-                log.info("创建萌新接待类型的聊天室断言成功")
-            except AssertionError as a:
-                log.error("创建萌新接待类型的聊天室断言失败")
-                common_func.save_webImgs("创建萌新接待类型聊天室-断言截图")
-                raise a
+    # @pytest.mark.success
+    # @allure.story('创建萌新接待类型的聊天室')
+    # @allure.title('创建一个萌新接待类型的聊天室')            
+    # def test_establish_reception_Room(self, startApp_keepUserData):
+    #     room_page = RoomPage(startApp_keepUserData)
+    #     common_func = Cb(startApp_keepUserData)
+    #     log.info("**************创建萌新接待类型的聊天室****************")
+    #     liveRoomMum = room_page.found_reception_room()
+    #     with allure.step("@创建萌新接待类型的聊天室@"):
+    #         try:
+    #             assert liveRoomMum == True
+    #             log.info("创建萌新接待类型的聊天室断言成功")
+    #         except AssertionError as a:
+    #             log.error("创建萌新接待类型的聊天室断言失败")
+    #             common_func.save_webImgs("创建萌新接待类型聊天室-断言截图")
+    #             raise a
 
 
         # log.info("***************首页进出关播人员主页*****************")
