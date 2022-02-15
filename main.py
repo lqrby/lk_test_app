1915c97e6c8639add0acf0773ba755f781d3ff68
lqrby <1640464937@qq.com>
2022-02-15 12:25:01 +0800

    main

--- main.py
+++ main.py
@@ -19,39 +19,43 @@ file_path = os.path.join(os.path.abspath('.'))
 file_path = file_path.replace('\\', '/')
 sys.path.append(file_path)
 from Common.log import get_logger
-from Common.getCpuAndMemory import DevicePerformanceMonitoring
+# from Common.getCpuAndMemory import DevicePerformanceMonitoring
 from Common.splicing import caps
 log = get_logger(logger_name="移动设备硬件资源监控日志")
 
-class ProcessMethod():
-    def __init__(self):
-        self.off = 1
-    #ui自动化入口 
-    def ui_main(self,p_name):
-        pytest.main(['-s', './Test_Cases', '--alluredir', "./allure_result/allure_temp"])
-        os.system("allure generate ./allure_result/allure_temp -o ./allure_result/{} --clean".format(ts))
-        os.system("allure generate ./allure_result/allure_temp -o ./allure_result/allure_report --clean")
-        os.system("7z a ./allure_result/{}.zip ./allure_result/{}".format(ts,ts))
-        log.info("xxxxxxxxxxxxxxxxxxxxxx结束了xxxxxxxxxxxxxxxxxxx")
+# class ProcessMethod():
+#     def __init__(self):
+#         self.off = 1
+#     #ui自动化入口 
+#     def ui_main(self,p_name):
+#         pytest.main(['-s', './Test_Cases', '--alluredir', "./allure_result/allure_temp"])
+#         os.system("allure generate ./allure_result/allure_temp -o ./allure_result/{} --clean".format(ts))
+#         os.system("allure generate ./allure_result/allure_temp -o ./allure_result/allure_report --clean")
+#         os.system("7z a ./allure_result/{}.zip ./allure_result/{}".format(ts,ts))
+#         log.info("xxxxxxxxxxxxxxxxxxxxxx结束了xxxxxxxxxxxxxxxxxxx")
     #移动设备资源监控
-    def resource_monitoring(self,p_name):
-        dpm = DevicePerformanceMonitoring()
-        while True:
-            dpm.all_device_information() #获取cpu和内存
+    # def resource_monitoring(self,p_name):
+    #     # dpm = DevicePerformanceMonitoring()
+    #     while True:
+    #         dpm.all_device_information() #获取cpu和内存
     
  
-class Life(multiprocessing.Process):
-    def __init__(self, life_type, p_name):
-        multiprocessing.Process.__init__(self)
-        self.life_type = life_type
-        self.p_name = p_name
+# class Life(multiprocessing.Process):
+#     def __init__(self, life_type, p_name):
+#         multiprocessing.Process.__init__(self)
+#         self.life_type = life_type
+#         self.p_name = p_name
  
-    def run(self):
-        self.life_type(self.p_name)
+#     def run(self):
+#         self.life_type(self.p_name)
  
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
     # pytest.main(['-s', './Test_Cases/test_homeMakeFriends.py', '--alluredir', "./allure_result/allure_temp"])
+    pytest.main(['-s', './Test_Cases', '--alluredir', "./allure_result/allure_temp"])
+    os.system("allure generate ./allure_result/allure_temp -o ./allure_result/{} --clean".format(ts))
+    os.system("allure generate ./allure_result/allure_temp -o ./allure_result/allure_report --clean")
+    os.system("7z a ./allure_result/{}.zip ./allure_result/{}".format(ts,ts))
     # dpm = DevicePerformanceMonitoring()
     # while True:
     #     # log.info("#################################################################")
@@ -62,17 +66,17 @@ class Life(multiprocessing.Process):
     #     time.sleep(3)
     
  
-if __name__ == '__main__':
-    start = time.time()
-    pm = ProcessMethod()
-    p1 = Life(pm.ui_main, "p1") 
-    p2 = Life(pm.resource_monitoring, "p2") 
-    p1.start()
-    p2.start()
-    p1.join()
-    p2.terminate()
-    end = time.time()
-    log.info("总共用时:{}".format(datetime.datetime.fromtimestamp(end)-datetime.datetime.fromtimestamp(start)))
+# if __name__ == '__main__':
+#     start = time.time()
+#     pm = ProcessMethod()
+#     p1 = Life(pm.ui_main, "p1") 
+#     p2 = Life(pm.resource_monitoring, "p2") 
+#     p1.start()
+#     p2.start()
+#     p1.join()
+#     p2.terminate()
+#     end = time.time()
+#     log.info("总共用时:{}".format(datetime.datetime.fromtimestamp(end)-datetime.datetime.fromtimestamp(start)))
 
 
 
